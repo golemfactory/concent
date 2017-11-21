df0ce07c0e26ac9056b85e684380d13552479c27
Kamil Śliwak <kamil.sliwak@codepoets.it>
2017-11-21 17:18:58 +0100

    core.views: Stubs for send(), receive() and receive_out_of_band() views

--- concent_api/core/urls.py
+++ concent_api/core/urls.py
@@ -1,4 +1,9 @@
 from django.conf.urls import url
 
+from .views import send, receive, receive_out_of_band
+
 urlpatterns = [
+    url(r'^send/$',                 send,                name = 'send'),
+    url(r'^receive/$',              receive,             name = 'receive'),
+    url(r'^receive-out-of-band/$',  receive_out_of_band, name = 'receive_out_of_band'),
 ]

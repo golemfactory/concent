6f04a67a8ff3deacf16467607807adb44fad1986
Jakub89 <jakubczachura@gmail.com>
2017-12-11 17:18:16 +0100

    Add storage_cluster_address variable to settings

--- concent_api/concent_api/system_check.py
+++ concent_api/concent_api/system_check.py
@@ -2,6 +2,8 @@ from django.core.checks     import Error
 from django.core.checks     import Warning  # pylint: disable=redefined-builtin
 from django.core.checks     import register
 from django.conf            import settings
+from django.core.validators import URLValidator
+from django.core.exceptions import ValidationError
 
 from concent_api.constants  import AVAILABLE_CONCENT_FEATURES
 
@@ -40,3 +42,26 @@ def check_settings_concent_features(app_configs, **kwargs):  # pylint: disable=u
             ))
 
     return errors_and_warnings
+
+
+@register()
+def check_settings_storage_cluster_address(app_configs, **kwargs):  # pylint: disable=unused-argument
+    if not hasattr(settings, 'STORAGE_CLUSTER_ADDRESS') and 'gatekeeper' in settings.CONCENT_FEATURES:
+        return [Error(
+            'STORAGE_CLUSTER_ADDRESS setting is not defined',
+            hint = 'Set STORAGE_CLUSTER_ADDRESS in your local_settings.py to the address of a Concent storage cluster that offers the /upload/ and /download/ endpoints.',
+            id   = 'concent.E010',
+        )]
+
+    if hasattr(settings, 'STORAGE_CLUSTER_ADDRESS'):
+        url_validator = URLValidator(schemes = ['http', 'https'])
+        try:
+            url_validator(settings.STORAGE_CLUSTER_ADDRESS)
+        except ValidationError as error:
+            return [Error(
+                'STORAGE_CLUSTER_ADDRESS is not a valid URL',
+                hint = '{}'.format(error),
+                id   = 'concent.E011',
+            )]
+
+    return []

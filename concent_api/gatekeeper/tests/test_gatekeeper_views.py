edeb758d2e371e7e3c4587fad39c87329a173fb2
Jakub89 <jakubczachura@gmail.com>
2017-12-19 12:16:47 +0100

    gatekeeper.views: Validate ConcentFileTransferToken

---
+++ concent_api/gatekeeper/tests/test_gatekeeper_views.py
@@ -0,0 +1,170 @@
+import datetime
+from base64         import b64encode
+
+from freezegun      import freeze_time
+from django.conf    import settings
+from django.http    import JsonResponse
+from django.test    import override_settings
+from django.test    import TestCase
+from django.urls    import reverse
+
+from golem_messages.shortcuts       import dump
+from golem_messages.message         import FileTransferToken
+
+
+@override_settings(
+    CONCENT_PRIVATE_KEY = b'l\xcdh\x19\xeb$>\xbcG\xa1\xc7v\xe8\xd7o\x0c\xbf\x0e\x0fM\x89lw\x1e\xd7K\xd6Hnv$\xa2',
+    CONCENT_PUBLIC_KEY  = b'\xf3\x97\x19\xcdX\xda\x86tiP\x1c&\xd39M\x9e\xa4\xddb\x89\xb5,]O\xd5cR\x84\xb85\xed\xc9\xa17e,\xb2s\xeb\n1\xcaN.l\xba\xc3\xb7\xc2\xba\xff\xabN\xde\xb3\x0b\xa6l\xbf6o\x81\xe0;',
+    STORAGE_CLUSTER_ADDRESS = 'http://devel.concent.golem.network/'
+)
+class GatekeeperViewUploadTest(TestCase):
+
+    @freeze_time("2018-12-30 11:00:00")
+    def setUp(self):
+        self.message_timestamp  = int(datetime.datetime.now().timestamp())
+        self.public_key         = '85cZzVjahnRpUBwm0zlNnqTdYom1LF1P1WNShLg17cmhN2UssnPrCjHKTi5susO3wrr/q07eswumbL82b4HgOw=='
+        self.upload_token                                 = FileTransferToken()
+        self.upload_token.token_expiration_deadline       = self.message_timestamp + 3600
+        self.upload_token.storage_cluster_address         = 'http://devel.concent.golem.network/'
+        self.upload_token.authorized_client_public_key    = b'\xf3\x97\x19\xcdX\xda\x86tiP\x1c&\xd39M\x9e\xa4\xddb\x89\xb5,]O\xd5cR\x84\xb85\xed\xc9\xa17e,\xb2s\xeb\n1\xcaN.l\xba\xc3\xb7\xc2\xba\xff\xabN\xde\xb3\x0b\xa6l\xbf6o\x81\xe0;'
+
+        self.upload_token.files                 = [FileTransferToken.FileInfo()]
+        self.upload_token.files[0]['path']      = 'blender/benchmark/test_task/scene-Helicopter-27-cycles.blend'
+        self.upload_token.files[0]['checksum']  = '098f6bcd4621d373cade4e832627b4f6'
+        self.upload_token.files[0]['size']      = 1024
+        self.upload_token.operation             = 'upload'
+
+    @freeze_time("2018-12-30 11:00:00")
+    def test_upload_should_accept_valid_message(self):
+        golem_upload_token = dump(self.upload_token, settings.CONCENT_PRIVATE_KEY, settings.CONCENT_PUBLIC_KEY)
+        encrypted_token = b64encode(golem_upload_token).decode()
+        response = self.client.post(
+            '{}{}'.format(
+                reverse('gatekeeper:upload'),
+                'blender/benchmark/test_task/scene-Helicopter-27-cycles.blend'
+            ),
+            HTTP_AUTHORIZATION = 'Golem ' + encrypted_token,
+            content_type = 'application/x-www-form-urlencoded',
+            HTTP_CONCENT_CLIENT_PUBLIC_KEY = self.public_key
+        )
+
+        self.assertIsInstance(response, JsonResponse)
+        self.assertEqual(response.status_code, 200)
+
+    @freeze_time("2018-12-30 11:00:00")
+    def test_upload_should_return_401_if_wrong_request_content_type(self):
+        golem_upload_token = dump(self.upload_token, settings.CONCENT_PRIVATE_KEY, settings.CONCENT_PUBLIC_KEY)
+        encrypted_token = b64encode(golem_upload_token).decode()
+        response = self.client.post(
+            '{}{}'.format(
+                reverse('gatekeeper:upload'),
+                'blender/benchmark/test_task/scene-Helicopter-27-cycles.blend'
+            ),
+            HTTP_AUTHORIZATION = 'Golem ' + encrypted_token,
+            content_type = '',
+            HTTP_CONCENT_CLIENT_PUBLIC_KEY = self.public_key
+        )
+
+        self.assertIsInstance(response, JsonResponse)
+        self.assertEqual(response.status_code, 401)
+        self.assertIn('message', response.json().keys())
+
+    @freeze_time("2018-12-30 12:00:01")
+    def test_upload_should_return_401_if_message_token_deadline_pass(self):
+        golem_upload_token = dump(self.upload_token, settings.CONCENT_PRIVATE_KEY, settings.CONCENT_PUBLIC_KEY)
+        encrypted_token = b64encode(golem_upload_token).decode()
+        response = self.client.post(
+            '{}{}'.format(
+                reverse('gatekeeper:upload'),
+                'blender/benchmark/test_task/scene-Helicopter-27-cycles.blend'
+            ),
+            HTTP_AUTHORIZATION = 'Golem ' + encrypted_token,
+            content_type = 'application/x-www-form-urlencoded',
+            HTTP_CONCENT_CLIENT_PUBLIC_KEY = self.public_key
+        )
+
+        self.assertIsInstance(response, JsonResponse)
+        self.assertEqual(response.status_code, 401)
+        self.assertIn('message', response.json().keys())
+
+
+@override_settings(
+    CONCENT_PRIVATE_KEY = b'l\xcdh\x19\xeb$>\xbcG\xa1\xc7v\xe8\xd7o\x0c\xbf\x0e\x0fM\x89lw\x1e\xd7K\xd6Hnv$\xa2',
+    CONCENT_PUBLIC_KEY  = b'\xf3\x97\x19\xcdX\xda\x86tiP\x1c&\xd39M\x9e\xa4\xddb\x89\xb5,]O\xd5cR\x84\xb85\xed\xc9\xa17e,\xb2s\xeb\n1\xcaN.l\xba\xc3\xb7\xc2\xba\xff\xabN\xde\xb3\x0b\xa6l\xbf6o\x81\xe0;',
+    STORAGE_CLUSTER_ADDRESS = 'http://devel.concent.golem.network/'
+)
+class GatekeeperViewDownloadTest(TestCase):
+
+    @freeze_time("2018-12-30 11:00:00")
+    def setUp(self):
+        self.message_timestamp  = int(datetime.datetime.now().timestamp())
+        self.public_key         = '85cZzVjahnRpUBwm0zlNnqTdYom1LF1P1WNShLg17cmhN2UssnPrCjHKTi5susO3wrr/q07eswumbL82b4HgOw=='
+        self.upload_token                                 = FileTransferToken()
+        self.upload_token.token_expiration_deadline       = self.message_timestamp + 3600
+        self.upload_token.storage_cluster_address         = 'http://devel.concent.golem.network/'
+        self.upload_token.authorized_client_public_key    = b'\xf3\x97\x19\xcdX\xda\x86tiP\x1c&\xd39M\x9e\xa4\xddb\x89\xb5,]O\xd5cR\x84\xb85\xed\xc9\xa17e,\xb2s\xeb\n1\xcaN.l\xba\xc3\xb7\xc2\xba\xff\xabN\xde\xb3\x0b\xa6l\xbf6o\x81\xe0;'
+
+        self.upload_token.files                 = [FileTransferToken.FileInfo()]
+        self.upload_token.files[0]['path']      = 'blender/benchmark/test_task/scene-Helicopter-27-cycles.blend'
+        self.upload_token.files[0]['checksum']  = '098f6bcd4621d373cade4e832627b4f6'
+        self.upload_token.files[0]['size']      = 1024
+        self.upload_token.operation             = 'download'
+
+    @freeze_time("2018-12-30 11:00:00")
+    def test_download_should_accept_valid_message(self):
+        golem_upload_token = dump(self.upload_token, settings.CONCENT_PRIVATE_KEY, settings.CONCENT_PUBLIC_KEY)
+        encrypted_token = b64encode(golem_upload_token).decode()
+        response = self.client.get(
+            '{}{}'.format(
+                reverse('gatekeeper:download'),
+                'blender/benchmark/test_task/scene-Helicopter-27-cycles.blend'
+            ),
+            HTTP_AUTHORIZATION = 'Golem ' + encrypted_token,
+            HTTP_CONCENT_CLIENT_PUBLIC_KEY = self.public_key
+        )
+
+        self.assertIsInstance(response, JsonResponse)
+        self.assertEqual(response.status_code, 200)
+
+    @freeze_time("2018-12-30 11:00:00")
+    def test_download_should_return_401_if_wrong_authorization_header(self):
+        golem_upload_token = dump(self.upload_token, settings.CONCENT_PRIVATE_KEY, settings.CONCENT_PUBLIC_KEY)
+        encrypted_token = b64encode(golem_upload_token).decode()
+        wrong_test_headers = [
+            {'HTTP_AUTHORIZATION':      'GolemGolem '+ encrypted_token},
+            {'HTTP_AUTHORIZATION_ABC':  'GolemGolem '+ encrypted_token},
+            {'HTTP_AUTHORIZATION':      ''},
+            {'HTTP_AUTHORIZATION':      'Golem encrypted_token '},
+            {'HTTP_AUTHORIZATION':      'Golem a' + encrypted_token}
+        ]
+        for headers in wrong_test_headers:
+            response = self.client.get(
+                '{}{}'.format(
+                    reverse('gatekeeper:download'),
+                    'blender/benchmark/test_task/scene-Helicopter-27-cycles.blend'
+                ),
+                **headers,
+                HTTP_CONCENT_CLIENT_PUBLIC_KEY = self.public_key
+            )
+            self.assertIsInstance(response, JsonResponse)
+            self.assertEqual(response.status_code, 401)
+            self.assertIn('message', response.json().keys())
+
+    @freeze_time("2018-12-30 11:00:00")
+    def test_download_should_return_401_if_wrong_token_cluster_address(self):
+        upload_token = self.upload_token
+        upload_token.storage_cluster_address = 'www://storage.concent.golem.network/'
+        golem_upload_token = dump(upload_token, settings.CONCENT_PRIVATE_KEY, settings.CONCENT_PUBLIC_KEY)
+        encrypted_token = b64encode(golem_upload_token).decode()
+        response = self.client.get(
+            '{}{}'.format(
+                reverse('gatekeeper:download'),
+                'blender/benchmark/test_task/scene-Helicopter-27-cycles.blend'
+            ),
+            HTTP_AUTHORIZATION = 'Golem ' + encrypted_token,
+            HTTP_CONCENT_CLIENT_PUBLIC_KEY = self.public_key
+        )
+
+        self.assertIsInstance(response, JsonResponse)
+        self.assertEqual(response.status_code, 401)
+        self.assertIn('message', response.json().keys())
--- concent_api/gatekeeper/views.py
+++ concent_api/gatekeeper/views.py
@@ -1,8 +1,13 @@
 import binascii
+import datetime
 from base64                         import b64decode
+from base64                         import b64encode
 
 from django.conf                    import settings
-from django.http                    import HttpResponse
+from django.http                    import JsonResponse
+from django.core.validators         import URLValidator
+from django.core.exceptions         import ValidationError
+from django.urls                    import reverse
 from django.views.decorators.csrf   import csrf_exempt
 from django.views.decorators.http   import require_POST
 from django.views.decorators.http   import require_safe
@@ -23,7 +28,7 @@ def upload(request):
     if response is not None:
         return response
 
-    return HttpResponse("", status = 200)
+    return JsonResponse({"message": "Request passed all upload validations."}, status = 200)
 
 
 @csrf_exempt
@@ -40,7 +45,7 @@ def download(request):
     if response is not None:
         return response
 
-    return HttpResponse("", status = 200)
+    return JsonResponse({"message": "Request passed all download validations."}, status = 200)
 
 
 def parse_headers(request):
@@ -65,7 +70,7 @@ def parse_headers(request):
         return gatekeeper_access_denied_response("Unable to decode token in the 'Authorization' header.")
 
     try:
-        loaded_golem_message = load(decoded_auth_header_content, settings.CONCENT_PRIVATE_KEY, settings.CONCENT_PUBLIC_KEY)
+        loaded_golem_message = load(decoded_auth_header_content, settings.CONCENT_PRIVATE_KEY, settings.CONCENT_PUBLIC_KEY, check_time = False)
     # FIXME: We want to catch only exceptions caused by malformed messages but golem-messages does not have specialized
     # exception classes for that. It simply raises AttributeError.
     except AttributeError:
@@ -74,5 +79,55 @@ def parse_headers(request):
     if loaded_golem_message is None:
         return gatekeeper_access_denied_response('Undefined golem message.')
     assert isinstance(loaded_golem_message, Message)
+
+    # Check if request header contains Concent-Client-Public-Key:
+    if 'HTTP_CONCENT_CLIENT_PUBLIC_KEY' not in request.META:
+        return gatekeeper_access_denied_response('Missing Concent-Client-Public-Key header.')
+
+    # Check ConcentFileTransferToken each field:
+    # -DEADLINE
+    if not isinstance(loaded_golem_message.token_expiration_deadline, int):
+        return gatekeeper_access_denied_response('Wrong type of token_expiration_deadline variable.')
+    current_time = int(datetime.datetime.now().timestamp())
+
+    if current_time > loaded_golem_message.token_expiration_deadline:
+        return gatekeeper_access_denied_response('token_expiration_deadline has passed.')
+
+    # -STORAGE_CLUSTER_ADDRESS
+    if not isinstance(loaded_golem_message.storage_cluster_address, str):
+        return gatekeeper_access_denied_response('Wrong type of storage_cluster_address variable.')
+    url_validator = URLValidator()
+    try:
+        url_validator(loaded_golem_message.storage_cluster_address)
+    except ValidationError:
+        return gatekeeper_access_denied_response('storage_cluster_address is not a valid URL.')
+    if loaded_golem_message.storage_cluster_address != settings.STORAGE_CLUSTER_ADDRESS:
+        return gatekeeper_access_denied_response('Given storage_cluster_address is not defined in settings.')
+
+    # -CLIENT_PUBLIC_KEY
+    if not isinstance(loaded_golem_message.authorized_client_public_key, bytes):
+        return gatekeeper_access_denied_response('Wrong type of authorized_client_public_key variable.')
+    client_public_key_base64 = (b64encode(loaded_golem_message.authorized_client_public_key)).decode('ascii')
+    if request.META['HTTP_CONCENT_CLIENT_PUBLIC_KEY'] != client_public_key_base64:
+        return gatekeeper_access_denied_response('authorized_client_public_key is different then Concent-Client-Public-Key header.')
+
+    # -OPERATION
+    if request.method == 'POST' and loaded_golem_message.operation != 'upload':
+        return gatekeeper_access_denied_response('Wrong operation variable for this request method.')
+    if request.method == 'GET' and loaded_golem_message.operation != 'download':
+        return gatekeeper_access_denied_response('Wrong operation variable for this request method.')
+
+    # -FILES
+    if not all(isinstance(file, dict) for file in loaded_golem_message.files):
+        return gatekeeper_access_denied_response('Wrong type of files variable.')
+    if request.method == 'POST':
+        path_to_file = request.get_full_path().partition(reverse('gatekeeper:upload'))[2]
+    if request.method == 'GET':
+        path_to_file = request.get_full_path().partition(reverse('gatekeeper:download'))[2]
+    transfer_token_paths_to_files = []
+    for file in loaded_golem_message.files:
+        transfer_token_paths_to_files.append(file['path'])
+    if path_to_file not in transfer_token_paths_to_files:
+        return gatekeeper_access_denied_response('Path to specified file is not listed in files variable.')
 
     return None

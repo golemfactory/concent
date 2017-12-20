fddc40937375de950ce47bdea09e8ce08950e722
Jakub89 <jakubczachura@gmail.com>
2017-12-20 16:54:55 +0100

    Use constants.py in system_check.py for better modularity

--- concent_api/concent_api/system_check.py
+++ concent_api/concent_api/system_check.py
@@ -3,27 +3,40 @@ from django.core.checks     import Warning  # pylint: disable=redefined-builtin
 from django.core.checks     import register
 from django.conf            import settings
 
+from concent_api.constants  import AVAILABLE_CONCENT_FEATURES
+
 
 @register()
 def check_settings_concent_features(app_configs, **kwargs):  # pylint: disable=unused-argument
 
     if not isinstance(settings.CONCENT_FEATURES, list):
-        return [Error('The value of CONCENT_FEATURES setting is not a list', id = 'concent.E001')]
+        return [Error(
+            'The value of CONCENT_FEATURES setting is not a list',
+            id = 'concent.E001',
+        )]
 
-    uknown_features = set(settings.CONCENT_FEATURES) - {"concent-api", "gatekeeper", "admin-panel"}
+    uknown_features = set(settings.CONCENT_FEATURES) - AVAILABLE_CONCENT_FEATURES.keys()
     if len(uknown_features) > 0:
-        return [Error('Unknown features specified CONCENT_FEATURES setting',               hint = 'Did you make a typo in the name?', id = 'concent.E002')]
+        return [Error(
+            'Unknown features specified CONCENT_FEATURES setting',
+            hint = 'Did you make a typo in the name?',
+            id   = 'concent.E002',
+        )]
 
+    errors_and_warnings = []
     if len(set(settings.CONCENT_FEATURES)) != len(settings.CONCENT_FEATURES):
-        return [Warning('Some features appear multiple times in CONCENT_FEATURES setting', hint = 'Remove the duplicate names.',      id = 'concent.W001')]
+        errors_and_warnings.append(Warning(
+            'Some features appear multiple times in CONCENT_FEATURES setting',
+            hint = 'Remove the duplicate names.',
+            id   = 'concent.W001',
+        ))
 
-    if "concent-api" in settings.CONCENT_FEATURES and "core"                 not in settings.INSTALLED_APPS:
-        return [Error("App 'core' required by 'concent-api' feature is not defined in INSTALLED_APPS",                 hint = "Add the missing app to INSTALLED_APPS.", id = 'concent.E004')]
+    for feature in settings.CONCENT_FEATURES:
+        if not set(AVAILABLE_CONCENT_FEATURES[feature]['required_django_apps']).issubset(set(settings.INSTALLED_APPS)):
+            errors_and_warnings.append(Error(
+                'Not all apps required by feature "{}" are enabled in INSTALLED_APPS'.format(feature),
+                hint = 'Add the following apps to INSTALLED_APPS: {}'.format(AVAILABLE_CONCENT_FEATURES[feature]['required_django_apps']),
+                id   = 'concent.E003',
+            ))
 
-    if "gatekeeper"  in settings.CONCENT_FEATURES  and "gatekeeper"           not in settings.INSTALLED_APPS:
-        return [Error("App 'gatekeeper' required by 'gatekeeper' feature is not defined in INSTALLED_APPS",            hint = "Add the missing app to INSTALLED_APPS.", id = 'concent.E005')]
-
-    if "admin-panel" in settings.CONCENT_FEATURES and "django.contrib.admin" not in settings.INSTALLED_APPS:
-        return [Error("App 'django.contrib.admin' required by 'admin-panel' feature is not defined in INSTALLED_APPS", hint = "Add the missing app to INSTALLED_APPS.", id = 'concent.E006')]
-
-    return []
+    return errors_and_warnings

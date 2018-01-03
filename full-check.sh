dbcfef9362621c2626737372a408d8b518dbd79f
Radoslaw Wrzesien <radoslaw.wrzesien@codepoets.it>
2018-01-03 13:25:02 +0100

    Add coverage report for django tests

--- full-check.sh
+++ full-check.sh
@@ -12,8 +12,11 @@ printf "========================= MYPY STATIC TYPE CHECKER =================\n"
 mypy --config-file=mypy.ini concent_api/
 printf "\n"
 
-printf "============================ UNIT TESTS ============================\n"
+printf "========================= UNIT TESTS WITH COVERAGE =================\n"
 # NOTE: 'manage.py test' does not find all tests unless we run it from within the app directory.
+rm -rf contrib/coverage/coverage_html
 cd concent_api/
-python3 manage.py test
+coverage run --rcfile=../coverage-config --source='.' manage.py test
+coverage html --rcfile=../coverage-config
+coverage report -m
 cd ..

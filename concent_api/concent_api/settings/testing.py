27cea7e7d35c71a7c88b1788f100c9290eb6cd82
Radoslaw Wrzesien <radoslaw.wrzesien@codepoets.it>
2018-01-09 02:03:26 +0100

    Separate settings for running tests

---
+++ concent_api/concent_api/settings/testing.py
@@ -0,0 +1,30 @@
+# pylint: disable=unused-wildcard-import
+from .base import *  # NOQA  # pylint: disable=wildcard-import
+
+SECRET_KEY = 'testkey'
+
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        # NAME intentionally left unset, in case tests try to access the production database.
+        # This could happen e.g. if someone executes a query at module level.
+        'ENGINE':          'django.db.backends.postgresql_psycopg2',
+        'USER':            'postgres',
+        'ATOMIC_REQUESTS': True,
+        'TEST': {
+            'NAME': 'test_concent_api'
+        }
+    }
+}
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'handlers': {
+        'null_handler': {'class': 'logging.NullHandler'}
+    },
+    'loggers': {
+        '': {'handlers': ['null_handler']}
+    }
+}

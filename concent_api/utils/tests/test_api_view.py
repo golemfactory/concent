b3b07e51159d4ab7855e45fdaae9227ac6416711
Kamil Śliwak <kamil.sliwak@codepoets.it>
2018-01-12 17:54:51 +0100

    utils.test_api_view: Replace hard-coded keys with key generator calls

--- concent_api/utils/tests/test_api_view.py
+++ concent_api/utils/tests/test_api_view.py
@@ -6,12 +6,16 @@ from django.conf            import settings
 from golem_messages.message import WantToComputeTask
 from golem_messages         import dump, load
 
-from utils.api_view import api_view
+from utils.api_view         import api_view
+from utils.testing_helpers  import generate_ecc_key_pair
+
+
+(CONCENT_PRIVATE_KEY, CONCENT_PUBLIC_KEY) = generate_ecc_key_pair()
 
 
 @override_settings(
-    CONCENT_PRIVATE_KEY = b'l\xcdh\x19\xeb$>\xbcG\xa1\xc7v\xe8\xd7o\x0c\xbf\x0e\x0fM\x89lw\x1e\xd7K\xd6Hnv$\xa2',
-    CONCENT_PUBLIC_KEY  = b'\xf3\x97\x19\xcdX\xda\x86tiP\x1c&\xd39M\x9e\xa4\xddb\x89\xb5,]O\xd5cR\x84\xb85\xed\xc9\xa17e,\xb2s\xeb\n1\xcaN.l\xba\xc3\xb7\xc2\xba\xff\xabN\xde\xb3\x0b\xa6l\xbf6o\x81\xe0;'
+    CONCENT_PRIVATE_KEY = CONCENT_PRIVATE_KEY,
+    CONCENT_PUBLIC_KEY  = CONCENT_PUBLIC_KEY,
 )
 class ApiViewTestCase(TestCase):
     def setUp(self):

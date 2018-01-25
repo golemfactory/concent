116fa91942462addaa5028102b38399d53674a5c
Radoslaw Wrzesien <radoslaw.wrzesien@codepoets.it>
2018-01-25 13:25:31 +0100

    Added cluster test

---
+++ concent_api/api-integration-force-get-task-result-test.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import datetime
+import time
+from base64                 import b64encode
+
+from golem_messages         import message
+
+from utils.testing_helpers  import generate_ecc_key_pair
+
+from api_testing_helpers    import api_request
+
+import requests
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "concent_api.settings")
+
+(PROVIDER_PRIVATE_KEY,  PROVIDER_PUBLIC_KEY)  = generate_ecc_key_pair()
+(REQUESTOR_PRIVATE_KEY, REQUESTOR_PUBLIC_KEY) = generate_ecc_key_pair()
+
+
+def parse_command_line(command_line):
+    if len(command_line) <= 1:
+        sys.exit('Not enough arguments')
+
+    if len(command_line) >= 3:
+        sys.exit('Too many arguments')
+
+    cluster_url = command_line[1]
+    return cluster_url
+
+
+def get_force_get_task_result(task_id, current_time, size, checksum):
+
+    compute_task_def = message.ComputeTaskDef()
+    compute_task_def['task_id'] = task_id
+    compute_task_def['deadline'] = current_time + 60
+    task_to_compute = message.TaskToCompute(
+        timestamp        = current_time,
+        compute_task_def = compute_task_def
+    )
+    report_computed_task = message.ReportComputedTask(
+        task_to_compute = task_to_compute,
+        size            = size,
+        checksum        = checksum,
+    )
+
+    force_get_task_result = message.concents.ForceGetTaskResult(
+        timestamp            = current_time - 3540,
+        report_computed_task = report_computed_task,
+    )
+
+    return force_get_task_result
+
+
+def main():
+    cluster_url  = parse_command_line(sys.argv)
+    current_time = int(datetime.datetime.now().timestamp())
+
+    # Case 1 - test for existing file
+    api_request(
+        cluster_url,
+        'send',
+        REQUESTOR_PRIVATE_KEY,
+        CONCENT_PUBLIC_KEY,
+        get_force_get_task_result(
+            1,
+            current_time,
+            size    = 1024,
+            checksum = '098f6bcd4621d373cade4e832627b4f6'
+        ),
+        headers = {
+            'Content-Type': 'application/octet-stream',
+            'concent-client-public-key': b64encode(REQUESTOR_PUBLIC_KEY).decode('ascii'),
+        }
+    )
+    time.sleep(10)
+
+    api_request(
+        cluster_url,
+        'receive',
+        PROVIDER_PRIVATE_KEY,
+        CONCENT_PUBLIC_KEY,
+        headers = {
+            'Content-Type': 'application/octet-stream',
+            'concent-client-public-key': b64encode(PROVIDER_PUBLIC_KEY).decode('ascii'),
+        }
+    )
+    time.sleep(10)
+
+    api_request(
+        cluster_url,
+        'receive',
+        REQUESTOR_PRIVATE_KEY,
+        CONCENT_PUBLIC_KEY,
+        headers = {
+            'Content-Type': 'application/octet-stream',
+            'concent-client-public-key': b64encode(REQUESTOR_PUBLIC_KEY).decode('ascii'),
+        }
+    )
+
+    # Case 2 - test for non existing file
+    api_request(
+        cluster_url,
+        'send',
+        REQUESTOR_PRIVATE_KEY,
+        CONCENT_PUBLIC_KEY,
+        get_force_get_task_result(
+            99999,
+            current_time,
+            size    = 1024,
+            checksum = '098f6bcd4621d373cade4e832627b4f6'
+        ),
+        headers = {
+            'Content-Type': 'application/octet-stream',
+            'concent-client-public-key': b64encode(REQUESTOR_PUBLIC_KEY).decode('ascii')
+        }
+    )
+    time.sleep(10)
+
+    api_request(
+        cluster_url,
+        'receive',
+        PROVIDER_PRIVATE_KEY,
+        CONCENT_PUBLIC_KEY,
+        headers = {
+            'Content-Type': 'application/octet-stream',
+            'concent-client-public-key': b64encode(PROVIDER_PUBLIC_KEY).decode('ascii')
+        }
+    )
+    time.sleep(10)
+
+    api_request(
+        cluster_url,
+        'receive',
+        REQUESTOR_PRIVATE_KEY,
+        CONCENT_PUBLIC_KEY,
+        headers = {
+            'Content-Type': 'application/octet-stream',
+            'concent-client-public-key': b64encode(REQUESTOR_PUBLIC_KEY).decode('ascii')
+        }
+    )
+
+
+if __name__ == '__main__':
+    try:
+        from concent_api.settings import CONCENT_PUBLIC_KEY
+        main()
+    except requests.exceptions.ConnectionError as exception:
+        print("\nERROR: Failed connect to the server.\n", file = sys.stderr)
+        sys.exit(str(exception))

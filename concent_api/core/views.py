eb96044c88042e809f10a02546feb5ffc70811a5
Radoslaw Wrzesien <radoslaw.wrzesien@codepoets.it>
2018-02-12 16:22:38 +0100

    Typo fix

--- concent_api/core/views.py
+++ concent_api/core/views.py
@@ -424,13 +424,13 @@ def handle_send_force_subtask_results(request, client_message: message.concents.
                 reason      = message.concents.ServiceRefused.REASON.TooSmallProviderDeposit,
             )
 
-    client_message_send_to_soon = client_message.ack_report_computed_task.timestamp + settings.SUBTASK_VERIFICATION_TIME
-    client_message_send_to_late = client_message.ack_report_computed_task.timestamp + settings.SUBTASK_VERIFICATION_TIME + settings.FORCE_ACCEPTANCE_TIME
-    if client_message_send_to_late < current_time:
+    client_message_send_too_soon = client_message.ack_report_computed_task.timestamp + settings.SUBTASK_VERIFICATION_TIME
+    client_message_send_too_late = client_message.ack_report_computed_task.timestamp + settings.SUBTASK_VERIFICATION_TIME + settings.FORCE_ACCEPTANCE_TIME
+    if client_message_send_too_late < current_time:
         return message.concents.ForceSubtaskResultsRejected(
             reason = message.concents.ForceSubtaskResultsRejected.REASON.RequestTooLate,
         )
-    elif current_time < client_message_send_to_soon:
+    elif current_time < client_message_send_too_soon:
         return message.concents.ForceSubtaskResultsRejected(
             reason = message.concents.ForceSubtaskResultsRejected.REASON.RequestPremature,
         )

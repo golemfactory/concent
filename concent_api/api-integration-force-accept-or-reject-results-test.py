5c50558b6186aed1118b7b395156b85044933726
Piotr Dybowski <piotr.dybowski@nokia.com>
2018-03-05 09:00:22 +0100

    Refactor - move duplicated code to api_testing_common

--- concent_api/api-integration-force-accept-or-reject-results-test.py
+++ concent_api/api-integration-force-accept-or-reject-results-test.py
@@ -12,7 +12,7 @@ from golem_messages         import message
 from utils.helpers          import get_current_utc_timestamp
 from utils.testing_helpers  import generate_ecc_key_pair
 
-from api_testing_common    import api_request
+from api_testing_common import api_request, parse_command_line
 from api_testing_common    import timestamp_to_isoformat
 
 import requests
@@ -21,17 +21,6 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "concent_api.settings")
 
 (PROVIDER_PRIVATE_KEY,  PROVIDER_PUBLIC_KEY)  = generate_ecc_key_pair()
 (REQUESTOR_PRIVATE_KEY, REQUESTOR_PUBLIC_KEY) = generate_ecc_key_pair()
-
-
-def parse_command_line(command_line):
-    if len(command_line) <= 1:
-        sys.exit('Not enough arguments')
-
-    if len(command_line) >= 3:
-        sys.exit('Too many arguments')
-
-    cluster_url = command_line[1]
-    return cluster_url
 
 
 def force_subtask_results(timestamp = None, ack_report_computed_task = None):

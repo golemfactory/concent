41b60b9b5162d25a0282a0f7915395a5fdc01de5
Radoslaw Wrzesien <radoslaw.wrzesien@codepoets.it>
2018-03-15 17:51:22 +0100

    [M] Remove unused models

--- concent_api/core/admin.py
+++ concent_api/core/admin.py
@@ -1,10 +1,6 @@
 from django.contrib import admin
 
 from .models import StoredMessage
-from .models import ReceiveStatus
-from .models import ReceiveOutOfBandStatus
 
 
 admin.site.register(StoredMessage)
-admin.site.register(ReceiveStatus)
-admin.site.register(ReceiveOutOfBandStatus)
--- concent_api/core/models.py
+++ concent_api/core/models.py
@@ -7,19 +7,16 @@ from django.db.models       import CharField
 from django.db.models       import DateTimeField
 from django.db.models       import DecimalField
 from django.db.models       import ForeignKey
-from django.db.models       import Manager
 from django.db.models       import Model
 from django.db.models       import OneToOneField
 from django.db.models       import PositiveSmallIntegerField
-from django.db.models       import Q
-from django.db.models       import QuerySet
+from django.db.models       import Manager
 from django.utils           import timezone
 
 from golem_messages         import message
 
 from utils.fields           import Base64Field
 from utils.fields           import ChoiceEnum
-from utils.helpers          import is_base64
 
 from .constants             import GOLEM_PUBLIC_KEY_LENGTH
 from .constants             import MESSAGE_TASK_ID_MAX_LENGTH
@@ -34,55 +31,6 @@ class StoredMessage(Model):
 
     def __str__(self):
         return 'StoredMessage #{}, type:{}, {}'.format(self.id, self.type, self.timestamp)
-
-
-class ReceiveStatusManager(Manager):
-
-    def filter_public_key(self, client_public_key: str) -> QuerySet:
-        """ Returns receive statuses matching client public. """
-        assert is_base64(client_public_key)
-        return self.filter(
-            Q(message__auth__provider_public_key  = client_public_key) |
-            Q(message__auth__requestor_public_key = client_public_key)
-        )
-
-
-class ReceiveStatus(Model):
-    message     = ForeignKey(StoredMessage)
-    timestamp   = DateTimeField()
-    delivered   = BooleanField(default = False)
-
-    objects = ReceiveStatusManager()
-
-    def __str__(self):
-        return 'ReceiveStatus #{}, message:{}'.format(self.id, self.message)
-
-    class Meta:
-        verbose_name        = ('Receive status')
-        verbose_name_plural = ('Receive statuses')
-
-
-class ReceiveOutOfBandStatus(Model):
-    message     = ForeignKey(StoredMessage)
-    timestamp   = DateTimeField()
-    delivered   = BooleanField(default = False)
-
-    def __str__(self):
-        return 'ReceiveOutOfBandStatus #{}, message:{}'.format(self.id, self.message)
-
-    class Meta:
-        verbose_name        = ('ReceiveOutOfBand status')
-        verbose_name_plural = ('ReceiveOutOfBand statuses')
-
-
-class MessageAuth(Model):
-    """
-    This class is used to store provider and requestor keys for message exchange related to given initial message.
-    """
-
-    message              = OneToOneField(StoredMessage, related_name = 'auth')
-    provider_public_key  = Base64Field(max_length=64)
-    requestor_public_key = Base64Field(max_length=64)
 
 
 class ClientManager(Manager):

a8ab9eba350896c756a9d89f399ae91312c5721d
MichalW-CP <michal.wyspianski@codepoets.it>
2018-03-21 12:25:02 +0100

    [WIP] get field names

--- concent_api/command_line_tool/message_extractor.py
+++ concent_api/command_line_tool/message_extractor.py
@@ -1,25 +1,42 @@
 import importlib
-import types
 import re
 from typing import Dict, Text, Any, List
 from golem_messages.message import Message
-from inspect import getmembers, ismodule, isclass
 
 JsonType = Dict[Text, Any]
 
-FIELD_NAMES = ['task_to_compute', 'compute_task_def', 'report_computed_task', 'force_get_task_result']
+
+# FIELD_NAMES = ['task_to_compute', 'compute_task_def', 'report_computed_task', 'force_get_task_result']
+
+def split_uppercase(message):
+    return (''.join([(word[:1].lower() + word[1:]) for word in re.sub(r'([A-Z])', r'_\1', message)]))[1:]
 
 
 def find_modules():
-    package = importlib.import_module("golem_messages.message.tasks")
-    package2 = importlib.import_module("golem_messages.message.concents")
-    package3 = importlib.import_module("golem_messages.message")
+    message_tasks = importlib.import_module("golem_messages.message.tasks")
+    message_concents = importlib.import_module("golem_messages.message.concents")
+    message_modules = [message_tasks, message_concents]
+    message_names = []
+    for module in message_modules:
+        module_items = module.__dict__.items()
+        message_list = list(
+            dict([(name, cls) for name, cls in module_items if isinstance(cls, type)]).keys())
+        for message in message_list:
+            message_names.append(message)
+            if message not in message_list:
+                message_names.append(message)
+    return message_names
 
-    return list(dict([(name, cls) for name, cls in package.__dict__.items() if isinstance(cls, type)]).keys())
+
+def get_field_names():
+    field_names = []
+    for message in find_modules():
+        field_names.append(split_uppercase(message))
+    return field_names
 
 
-# print(find_modules())
-# package = importlib.import_module("golem_messages.message.tasks")
+FIELD_NAMES = get_field_names()
+
 
 def validate_message_list(message_list: List[Message]) -> None:
     if len(message_list) > 1:
@@ -52,10 +69,6 @@ def substitue_message(json: JsonType, message_name: str, message: Message) -> Js
 
 def convert_message_name(message):
     return ''.join([(word[:1].capitalize() + word[1:]) for word in message.split('_')])
-
-
-def split_uppercase(message):
-    return (''.join([(word[:1].lower() + word[1:]) for word in re.sub(r'([A-Z])', r'_\1', message)]))[1:]
 
 
 class MessageExtractor(object):

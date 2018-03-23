36821d340fdde0b758630821d1310af5def18395
bartoszbetka <bartoszbetka94@gmail.com>
2018-03-23 12:39:14 +0100

    Add celery initialization to concent_api/__init__.py for gunicorn

--- concent_api/concent_api/__init__.py
+++ concent_api/concent_api/__init__.py
@@ -1,1 +1,7 @@
 default_app_config = 'concent_api.apps.ConcentApiConfig'
+
+# This creates the 'app' object for celery. Doing it here ensures that it's always created when Django starts.
+# It's not necessary when using `manage.py runserver` or `celery worker` but e.g. gunicorn needs it.
+from .celery import app as celery_app
+
+__all__ = ['celery_app']

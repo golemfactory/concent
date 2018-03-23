5436ec02170a1398ef668b167c2de8652efbf264
MichalW-CP <michal.wyspianski@codepoets.it>
2018-03-23 16:48:24 +0100

    Add get_protocol_constants

---
+++ concent_api/get_protocol_constants.py
@@ -0,0 +1,39 @@
+import sys
+import requests
+from typing import NamedTuple
+
+ProtocolConstants = NamedTuple(
+    "ProtocolConstants",
+    [
+        ("concent_messaging_time", int),
+        ("subtask_verification_time", int),
+        ("force_acceptance_time", int),
+        ("maximum_download_time", int),
+    ]
+)
+
+
+def get_protocol_constants(cluster_url):
+    url = f"{cluster_url}/api/v1/protocol-constants/"
+    response = requests.get(url, verify = False)
+    json = response.json()
+    return ProtocolConstants(**json)
+
+
+def print_protocol_constants(constants):
+    print("PROTOCOL_CONSTANTS: ")
+    print("\n".join(f"{field} = {getattr(constants, field)}" for field in constants._fields))
+
+
+def main():
+    try:
+        cluster_url = sys.argv[1]
+    except IndexError:
+        cluster_url = 'https://devel.concent.golem.network'
+
+    cluster_consts = get_protocol_constants(cluster_url)
+    print_protocol_constants(cluster_consts)
+
+
+if __name__ == '__main__':
+    main()

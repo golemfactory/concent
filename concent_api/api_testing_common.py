59d683e07a746012d032e3cbf095e1b8f0c6e8d1
Piotr Dybowski <piotr.dybowski@nokia.com>
2018-03-28 12:43:43 +0200

    Add helper functions to api_testing_common and refactor api_request

--- concent_api/api_testing_common.py
+++ concent_api/api_testing_common.py
@@ -9,7 +9,21 @@ import requests
 import http.client
 
 
-def print_golem_message(message, private_key, public_key, indent = 4):
+class TestAssertionException(Exception):
+    pass
+
+
+def assert_condition(actual, expected, error_message = '', function = None):
+    message = error_message or f"Actual: {actual} != expected: {expected}"
+    if function is not None:
+        if not function(actual, expected):
+            raise TestAssertionException(message)
+    else:
+        if actual != expected:
+            raise TestAssertionException(message)
+
+
+def print_golem_message(message, indent = 4):
     assert isinstance(message, Message)
     HEADER_FIELDS  = ['timestamp', 'encrypted', 'sig']
     PRIVATE_FIELDS = {'_payload', '_raw'}
@@ -23,62 +37,125 @@ def print_golem_message(message, private_key, public_key, indent = 4):
 
     for field, value in zip(fields, values):
         if isinstance(value, Message):
-            print_golem_message(value, private_key, public_key, indent = indent + 4)
+            print_golem_message(value, indent = indent + 4)
         else:
             print('{}{:30} = {}'.format(' ' * indent, field, value))
 
 
-def api_request(host, endpoint, private_key, public_key, data = None, headers = None):
-    assert all(value not in ['', None] for value in [endpoint, host, headers])
-    url = "{}/api/v1/{}/".format(host, endpoint)
+def validate_response_status(actual_status_code, expected_status):
+    if expected_status is not None:
+        assert_condition(
+            actual_status_code,
+            expected_status,
+            f"Expected:HTTP{expected_status}, actual:HTTP{actual_status_code}"
+        )
 
-    if data is None:
-        print('RECEIVE ({})'.format(url))
-    else:
-        print('SEND ({})'.format(url))
-        print('MESSAGE:')
-        print_golem_message(data, private_key, public_key)
 
-        data = dump(
+def validate_response_message(encoded_message, expected_message, private_key, public_key):
+    if expected_message is not None:
+        decoded_message = try_to_decode_golem_message(private_key, public_key, encoded_message)
+        assert_condition(
+            decoded_message,
+            expected_message,
+            error_message  = f"Expected:{expected_message}, actual:{decoded_message}",
+            function       = isinstance
+        )
+
+
+def validate_content_type(actual_content_type, expected_content_type):
+    if expected_content_type is not None:
+        assert_condition(
+            actual_content_type,
+            expected_content_type,
+            f"Wrong content type for Golem Message: {actual_content_type}"
+        )
+
+
+def api_request(host, endpoint, private_key, public_key,
+                data                    = None,
+                headers                 = None,
+                expected_status         = None,
+                expected_message        = None,
+                expected_content_type   = None
+):
+    def _prepare_data(data):
+        if data is None:
+            return ''
+        return dump(
             data,
             private_key,
             public_key,
         )
 
-    if data is None:
-        response = requests.post("{}".format(url), headers = headers, data = '')
-    else:
-        response = requests.post("{}".format(url), headers = headers, data = data)
+    def _print_data(data, url):
+        if data is None:
+            print('RECEIVE ({})'.format(url))
+        else:
+            print('SEND ({})'.format(url))
+            print('MESSAGE:')
+            print_golem_message(data)
 
+    assert all(value not in ['', None] for value in [endpoint, host, headers])
+    url         = "{}/api/v1/{}/".format(host, endpoint)
+
+    _print_data(data, url)
+
+    response    = requests.post("{}".format(url), headers = headers, data = _prepare_data(data), verify = False)
+
+    _print_response(private_key, public_key, response)
+    validate_response_status(response.status_code, expected_status)
+    validate_content_type(response.headers['Content-Type'], expected_content_type)
+    validate_response_message(response.content, expected_message, private_key, public_key)
+    print()
+
+
+def _print_response(private_key, public_key, response):
     if response.content is None:
         print('RAW RESPONSE: Reponse content is None')
     elif len(response.content) != 0:
-        print('STATUS: {} {}'.format(response.status_code, http.client.responses[response.status_code]))
-        print('MESSAGE:')
-        if response.headers['Content-Type'] == 'application/octet-stream':
-            try:
-                decoded_response = load(
-                    response.content,
-                    private_key,
-                    public_key,
-                    check_time = False
-                )
-            except MessageError as exception:
-                print("Failed to decode a Golem Message.")
-
-            print_golem_message(decoded_response, private_key, public_key)
-        elif response.headers['Content-Type'] == 'application/json':
-            try:
-                print(response.json())
-            except json.decoder.JSONDecodeError:
-                print('RAW RESPONSE: Failed to decode response content')
-        else:
-            print('RAW RESPONSE: Unexpected content-type of response message')
+        _print_message_from_response(private_key, public_key, response)
     else:
         print('STATUS: {} {}'.format(response.status_code, http.client.responses[response.status_code]))
         if response.text not in ['', None]:
             print('RAW RESPONSE: {}'.format(response.text))
-    print()
+
+
+def _print_message_from_response(private_key, public_key, response):
+    print('STATUS: {} {}'.format(response.status_code, http.client.responses[response.status_code]))
+    print('MESSAGE:')
+    print('Concent-Golem-Messages-Version = {}'.format(response.headers['concent-golem-messages-version']))
+    if response.headers['Content-Type'] == 'application/octet-stream':
+        _print_message_from_stream(private_key, public_key, response.content)
+    elif response.headers['Content-Type'] == 'application/json':
+        _print_message_from_json(response)
+    else:
+        print('RAW RESPONSE: Unexpected content-type of response message')
+
+
+def _print_message_from_json(response):
+    try:
+        print(response.json())
+    except json.decoder.JSONDecodeError:
+        print('RAW RESPONSE: Failed to decode response content')
+
+
+def _print_message_from_stream(private_key, public_key, content):
+    decoded_response = try_to_decode_golem_message(private_key, public_key, content)
+    print_golem_message(decoded_response)
+
+
+def try_to_decode_golem_message(private_key, public_key, content):
+    try:
+        decoded_response = load(
+            content,
+            private_key,
+            public_key,
+            check_time = False
+        )
+    except MessageError:
+        print("Failed to decode a Golem Message.")
+        raise
+    return decoded_response
 
 
 def timestamp_to_isoformat(timestamp):

ba4b94275890ebbb84224001df10a4d2d3277c33
Radoslaw Wrzesien <radoslaw.wrzesien@codepoets.it>
2018-03-29 13:01:51 +0200

    Add loading only tasks from selected concent features

--- concent_api/concent_api/celery.py
+++ concent_api/concent_api/celery.py
@@ -1,5 +1,8 @@
+import importlib
 import os
 from celery import Celery
+
+import django
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'concent_api.settings')
@@ -13,5 +16,15 @@ app = Celery('concent_api')
 #   should have a `CELERY_` prefix.
 app.config_from_object('django.conf:settings', namespace = 'CELERY')
 
-# Load task modules from all registered Django app configs.
-app.autodiscover_tasks()
+django.setup()
+from django.conf            import settings  # noqa: E402 pylint: disable=wrong-import-position
+from concent_api.constants  import AVAILABLE_CONCENT_FEATURES  # noqa: E402 pylint: disable=wrong-import-position
+
+# Load tasks only from features specified in CONCENT_FEATURES variable
+for feature_name in settings.CONCENT_FEATURES:
+
+    app.conf.imports += tuple(
+        ['{}.tasks'.format(app)
+         for app in AVAILABLE_CONCENT_FEATURES[feature_name]['required_django_apps']
+         if importlib.util.find_spec('{}.tasks'.format(app)) is not None]
+    )

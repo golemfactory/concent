b84573dc3410d565a335da166960a78b4d471311
Radoslaw Wrzesien <radoslaw.wrzesien@codepoets.it>
2018-04-16 13:18:01 +0200

    Fix for 'Concent crashes if ForceSubtaskResultsResponse does not contain TaskToCompute

--- concent_api/core/tests/test_integration_accept_or_reject_results.py
+++ concent_api/core/tests/test_integration_accept_or_reject_results.py
@@ -1168,6 +1168,65 @@ class AcceptOrRejectIntegrationTest(ConcentIntegrationTestCase):
 
         self._assert_client_count_is_equal(0)
 
+    def test_subtask_results_accepted_without_task_to_compute_should_return_http_400(self):
+        """
+        Test if Provider want to submit ForceSubtaskResults with SubtaskResultsAccepted without TaskToCompute.
+
+        Expected message exchange:
+        Requestor   -> Concent:     ForceSubtaskResultsResponse
+        Concent     -> Provider:    HTTP 400
+        """
+
+        serialized_force_subtask_results_response = self._get_serialized_force_subtask_results_response(
+            requestor_private_key=self.REQUESTOR_PRIVATE_KEY,
+            subtask_results_accepted=self._get_deserialized_subtask_results_accepted(
+                payment_ts="2018-02-05 11:00:01",
+            )
+        )
+
+        response_1 = self.client.post(
+            reverse('core:send'),
+            data= serialized_force_subtask_results_response,
+            content_type= 'application/octet-stream',
+            HTTP_CONCENT_CLIENT_PUBLIC_KEY= self._get_encoded_requestor_public_key(),
+            HTTP_CONCENT_OTHER_PARTY_PUBLIC_KEY=self._get_encoded_provider_public_key(),
+        )
+
+        self._test_400_response(response_1)
+        self._assert_stored_message_counter_not_increased()
+
+        self._assert_client_count_is_equal(0)
+
+    def test_subtask_results_rejected_without_task_to_compute_should_return_http_400(self):
+        """
+        Test if Provider want to submit ForceSubtaskResults with SubtaskResultsRejected without TaskToCompute.
+
+        Expected message exchange:
+        Requestor   -> Concent:     ForceSubtaskResultsResponse
+        Concent     -> Provider:    HTTP 400
+        """
+
+        serialized_force_subtask_results_response = self._get_serialized_force_subtask_results_response(
+            requestor_private_key=self.REQUESTOR_PRIVATE_KEY,
+            subtask_results_rejected=self._get_deserialized_subtask_results_rejected(
+                reason=message.tasks.SubtaskResultsRejected.REASON.VerificationNegative,
+                report_computed_task=message.ReportComputedTask()
+            )
+        )
+
+        response_1 = self.client.post(
+            reverse('core:send'),
+            data= serialized_force_subtask_results_response,
+            content_type= 'application/octet-stream',
+            HTTP_CONCENT_CLIENT_PUBLIC_KEY=self._get_encoded_requestor_public_key(),
+            HTTP_CONCENT_OTHER_PARTY_PUBLIC_KEY=self._get_encoded_provider_public_key(),
+        )
+
+        self._test_400_response(response_1)
+        self._assert_stored_message_counter_not_increased()
+
+        self._assert_client_count_is_equal(0)
+
     def test_requestor_doesnt_provide_response_should_end_with_subtask_results_settled_received_from_concent(self):
         """
         Expected message exchange:

f6e9b4cba6d57c0e0eaad5055bd07367ff46ac3b
MichalW-CP <michal.wyspianski@codepoets.it>
2018-04-18 16:26:12 +0200

    Test refactored, fixed file upload

--- concent_api/api-integration-force-get-task-result-test.py
+++ concent_api/api-integration-force-get-task-result-test.py
@@ -3,7 +3,6 @@
 import os
 import sys
 import hashlib
-import time
 import random
 from base64 import b64encode
 
@@ -16,13 +15,19 @@ from utils.helpers import sign_message
 from utils.testing_helpers import generate_ecc_key_pair
 
 from api_testing_common import api_request
+from api_testing_common import assert_condition
 from api_testing_common import create_client_auth_message
-from api_testing_common import parse_command_line
+from api_testing_common import count_fails
+from api_testing_common import execute_tests
+from api_testing_common import get_task_id_and_subtask_id
+from api_testing_common import get_tests_list
+from api_testing_common import parse_arguments
 from api_testing_common import timestamp_to_isoformat
 
 from freezegun import freeze_time
 
 from protocol_constants import get_protocol_constants
+from protocol_constants import print_protocol_constants
 
 import requests
 
@@ -30,44 +35,6 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "concent_api.settings")
 
 (PROVIDER_PRIVATE_KEY,  PROVIDER_PUBLIC_KEY)  = generate_ecc_key_pair()
 (REQUESTOR_PRIVATE_KEY, REQUESTOR_PUBLIC_KEY) = generate_ecc_key_pair()
-
-
-def upload_new_file_on_cluster(task_id, subtask_id, cluster_consts, current_time):
-
-    file_content    = task_id
-    file_size       = len(file_content)
-    file_check_sum  = 'sha1:' + hashlib.sha1(file_content.encode()).hexdigest()
-    file_path       = get_storage_file_path(task_id, subtask_id)
-
-    file_transfer_token = message.FileTransferToken()
-    file_transfer_token.token_expiration_deadline = int(
-        current_time +
-        (cluster_consts.concent_messaging_time * 3) +
-        (cluster_consts.maximum_download_time * 2)
-    )
-    file_transfer_token.storage_cluster_address         = STORAGE_CLUSTER_ADDRESS
-    file_transfer_token.authorized_client_public_key    = CONCENT_PUBLIC_KEY
-    file_transfer_token.operation                       = message.FileTransferToken.Operation.upload
-
-    file_transfer_token.files                   = [message.FileTransferToken.FileInfo()]
-    file_transfer_token.files[0]['path']        = file_path
-    file_transfer_token.files[0]['checksum']    = file_check_sum
-    file_transfer_token.files[0]['size']        = file_size
-
-    upload_token    = shortcuts.dump(file_transfer_token, CONCENT_PRIVATE_KEY, CONCENT_PUBLIC_KEY)
-    encrypted_token = b64encode(upload_token).decode()
-
-    authorized_golem_transfer_token = 'Golem ' + encrypted_token
-
-    headers = {
-        'Authorization': authorized_golem_transfer_token,
-        'Concent-Auth': b64encode(create_client_auth_message(CONCENT_PRIVATE_KEY, CONCENT_PUBLIC_KEY, CONCENT_PUBLIC_KEY)).decode(),
-        'Concent-upload-path': file_path,
-        'Content-Type': 'application/octet-stream'
-    }
-
-    response = requests.post("{}".format(STORAGE_CLUSTER_ADDRESS + 'upload/'), headers = headers, data = file_content, verify = False)
-    return (response.status_code, file_size, file_check_sum)
 
 
 def get_force_get_task_result(task_id, subtask_id, current_time, cluster_consts, size, package_hash, provider_public_key = None, requestor_public_key = None):
@@ -99,23 +66,54 @@ def get_force_get_task_result(task_id, subtask_id, current_time, cluster_consts,
     return force_get_task_result
 
 
-def main():
-    cluster_url     = parse_command_line(sys.argv)
-    current_time    = get_current_utc_timestamp()
-    subtask_id      = str(random.randrange(1, 100000))
-    task_id         = subtask_id + 'existing_file'
-    cluster_consts  = get_protocol_constants(cluster_url)
-
-    (response_status_code, file_size, file_check_sum) = upload_new_file_on_cluster(
-        task_id,
-        subtask_id,
-        cluster_consts,
-        current_time,
+def upload_file_to_storage_cluster(file_content, file_path, upload_token):
+    dumped_upload_token = shortcuts.dump(upload_token, None, CONCENT_PUBLIC_KEY)
+    b64_encoded_token = b64encode(dumped_upload_token).decode()
+    headers = {
+        'Authorization': 'Golem ' + b64_encoded_token,
+        'Concent-Auth': b64encode(
+            create_client_auth_message(PROVIDER_PRIVATE_KEY, PROVIDER_PUBLIC_KEY, CONCENT_PUBLIC_KEY)).decode(),
+        'Concent-upload-path': file_path,
+        'Content-Type': 'application/octet-stream'
+    }
+    return requests.post(
+        "{}upload/".format(STORAGE_CLUSTER_ADDRESS),
+        headers=headers,
+        data=file_content,
+        verify=False
     )
-    if response_status_code == 200:
-        print('\nCreated file with task_id {}. Checksum of this file is {}, and size of this file is {}.\n'.format(task_id, file_check_sum, file_size))
-    else:
-        print('File has not been stored on cluster')
+
+
+def main():
+    (cluster_url, patterns) = parse_arguments()
+    cluster_consts  = get_protocol_constants(cluster_url)
+    print_protocol_constants(cluster_consts)
+    test_id = str(random.randrange(1, 100000))
+
+    tests_to_execute = get_tests_list(patterns, list(globals().keys()))
+    execute_tests(
+        tests_to_execute=tests_to_execute,
+        objects=globals(),
+        cluster_consts=cluster_consts,
+        cluster_url=cluster_url,
+        test_id=test_id,
+    )
+
+    if count_fails.get_fails() > 0:
+        count_fails.print_fails()
+    print("END")
+
+
+@count_fails
+def test_case_1_test_for_existing_file(cluster_consts, cluster_url, test_id):
+    current_time    = get_current_utc_timestamp()
+    (subtask_id, task_id) = get_task_id_and_subtask_id(test_id, 'existing_file')
+
+    file_content = task_id
+    file_size = len(file_content)
+    file_check_sum = 'sha1:' + hashlib.sha1(file_content.encode()).hexdigest()
+    file_path = get_storage_file_path(task_id, subtask_id)
+
     # Case 1 - test for existing file
     api_request(
         cluster_url,
@@ -131,15 +129,14 @@ def main():
             package_hash = file_check_sum,
         ),
         headers = {
-            'Content-Type':                     'application/octet-stream',
+            'Content-Type': 'application/octet-stream',
         },
         expected_status=200,
         expected_message_type=message.concents.AckForceGetTaskResult.TYPE,
         expected_content_type='application/octet-stream',
     )
-    time.sleep(10)
 
-    api_request(
+    force_get_task_result_upload = api_request(
         cluster_url,
         'receive',
         PROVIDER_PRIVATE_KEY,
@@ -152,7 +149,19 @@ def main():
         expected_message_type=message.concents.ForceGetTaskResultUpload.TYPE,
         expected_content_type='application/octet-stream',
     )
-    time.sleep(10)
+
+    response = upload_file_to_storage_cluster(
+        file_content,
+        file_path,
+        force_get_task_result_upload.file_transfer_token
+    )
+
+    assert_condition(response.status_code, 200, 'File has not been stored on cluster')
+    print('\nUploaded file with task_id {}. Checksum of this file is {}, and size of this file is {}.\n'.format(
+        task_id,
+        file_check_sum,
+        file_size
+    ))
 
     api_request(
         cluster_url,
@@ -168,9 +177,12 @@ def main():
         expected_content_type='application/octet-stream',
     )
 
-    # Case 2 - test for non existing file
-    subtask_id      = str(random.randrange(1, 100000))
-    task_id = subtask_id + 'non_existing_file'
+
+@count_fails
+def test_case_2_test_for_non_existing_file(cluster_consts, cluster_url, test_id):
+    current_time    = get_current_utc_timestamp()
+    (subtask_id, task_id) = get_task_id_and_subtask_id(test_id, 'non_existing_file')
+
     api_request(
         cluster_url,
         'send',
@@ -191,7 +203,6 @@ def main():
         expected_message_type=message.concents.AckForceGetTaskResult.TYPE,
         expected_content_type='application/octet-stream',
     )
-    time.sleep(10)
 
     api_request(
         cluster_url,
@@ -206,7 +217,6 @@ def main():
         expected_message_type=message.concents.ForceGetTaskResultUpload.TYPE,
         expected_content_type='application/octet-stream',
     )
-    time.sleep(10)
 
     api_request(
         cluster_url,
@@ -223,7 +233,7 @@ def main():
 
 if __name__ == '__main__':
     try:
-        from concent_api.settings import CONCENT_PUBLIC_KEY, CONCENT_PRIVATE_KEY, STORAGE_CLUSTER_ADDRESS
+        from concent_api.settings import CONCENT_PUBLIC_KEY, STORAGE_CLUSTER_ADDRESS
         main()
     except requests.exceptions.ConnectionError as exception:
         print("\nERROR: Failed connect to the server.\n", file = sys.stderr)

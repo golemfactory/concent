2a92743c57d111f4a2bac0b147e2bc97da8a61f1
Radoslaw Wrzesien <radoslaw.wrzesien@codepoets.it>
2018-05-08 17:12:56 +0200

    Replace usage of SUBTASK_VERIFICATION_TIME with function calculating it

--- concent_api/core/message_handlers.py
+++ concent_api/core/message_handlers.py
@@ -40,6 +40,7 @@ from core.validation import validate_report_computed_task_time_window
 from core.validation import validate_task_to_compute
 from utils import logging
 from utils.helpers import calculate_maximum_download_time
+from utils.helpers import calculate_subtask_verification_time
 from utils.helpers import deserialize_message
 from utils.helpers import get_current_utc_timestamp
 from utils.helpers import parse_timestamp_to_utc_datetime
@@ -394,11 +395,11 @@ def handle_send_force_subtask_results(client_message: message.concents.ForceSubt
 
     verification_deadline       = (
         client_message.ack_report_computed_task.report_computed_task.task_to_compute.compute_task_def['deadline'] +
-        settings.SUBTASK_VERIFICATION_TIME
+        calculate_subtask_verification_time(client_message.ack_report_computed_task.report_computed_task)
     )
     forcing_acceptance_deadline = (
         client_message.ack_report_computed_task.report_computed_task.task_to_compute.compute_task_def['deadline'] +
-        settings.SUBTASK_VERIFICATION_TIME +
+        calculate_subtask_verification_time(client_message.ack_report_computed_task.report_computed_task) +
         settings.FORCE_ACCEPTANCE_TIME
     )
     if forcing_acceptance_deadline < current_time:
@@ -499,7 +500,7 @@ def handle_send_force_subtask_results_response(client_message):
 
     acceptance_deadline = (
         task_to_compute.compute_task_def['deadline'] +
-        settings.SUBTASK_VERIFICATION_TIME +
+        calculate_subtask_verification_time(report_computed_task) +
         settings.FORCE_ACCEPTANCE_TIME +
         settings.CONCENT_MESSAGING_TIME
     )

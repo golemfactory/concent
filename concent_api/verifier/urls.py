87a7a14cd941a5ba7663b13e85c7c7534633e8d7
Radoslaw Wrzesien <radoslaw.wrzesien@codepoets.it>
2018-05-09 02:25:49 +0200

    Hotfix for mypy warning

--- concent_api/verifier/urls.py
+++ concent_api/verifier/urls.py
@@ -1,1 +1,4 @@
-urlpatterns = []
+from mypy.types import Any  # noqa  # pylint: disable=unused-import
+from mypy.types import List  # noqa  # pylint: disable=unused-import
+
+urlpatterns = []  # type: List[Any]

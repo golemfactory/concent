d75d26902face337f148d6d30bd995d66d4b0d8b
MichalW-CP <michal.wyspianski@codepoets.it>
2018-05-15 12:40:30 +0200

    Add shutdown admin bar

--- concent_api/concent_api/settings/base.py
+++ concent_api/concent_api/settings/base.py
@@ -49,7 +49,7 @@ ROOT_URLCONF = 'concent_api.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'core', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -136,7 +136,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static-root')
---
+++ concent_api/core/templatetags/admin_tags.py
@@ -0,0 +1,39 @@
+from django import template
+from constance import config
+from core.models import Subtask
+
+register = template.Library()
+
+
+def get_active_subtasks():
+    active_state_names = [state.name for state in Subtask.ACTIVE_STATES]
+    return Subtask.objects.filter(state__in=active_state_names)
+
+
+def get_result_uploaded_subtasks():
+    return Subtask.objects.filter(state=Subtask.SubtaskState.RESULT_UPLOADED.name)  # pylint: disable=no-member
+
+
+@register.assignment_tag
+def get_shutdown_mode_state():
+    return config.SOFT_SHUTDOWN_MODE
+
+
+@register.assignment_tag
+def get_active_subtasks_amount():
+    return get_active_subtasks().count()
+
+
+@register.assignment_tag
+def get_active_subtasks_status():
+    return get_result_uploaded_subtasks().exists()
+
+
+@register.assignment_tag
+def get_result_uploaded_subtasks_amount():
+    return get_result_uploaded_subtasks().count()
+
+
+@register.assignment_tag
+def result_uploaded_subtasks_status():
+    return not get_active_subtasks_status() and get_result_uploaded_subtasks_amount()

ba2838e0cb1900e7212814401f96ba7e98841501
Pawel Kisielewicz <pawel.kisielewicz0@gmail.comgit config --global user.email pawel.kisielewicz@codepoets.it>
2018-05-21 09:48:15 +0200

    Add and update logging in gatekeeper. INFO for regular-positive way, WARNING for negative infos

--- concent_api/gatekeeper/utils.py
+++ concent_api/gatekeeper/utils.py
@@ -1,16 +1,35 @@
 from django.http import JsonResponse
+from golem_messages.message import FileTransferToken
+
+from utils import logging
 
 
-def gatekeeper_access_denied_response(message, error_code = None, path = None, subtask_id = None, client_key = None):
+def gatekeeper_access_denied_response(
+    message: str,
+    operation: FileTransferToken.Operation,
+    error_code=None,
+    path=None,
+    subtask_id=None,
+    client_key=None
+):
     data = {
-        'message':         message,
-        'error_code':      error_code.value,
-        'path_to_file':    path,
-        'subtask_id':      subtask_id,
-        'client_key':      client_key,
+        'message': message,
+        'error_code': error_code.value,
+        'path_to_file': path,
+        'subtask_id': subtask_id,
+        'client_key': client_key,
     }
 
+    logging.log_operation_validation_failed(
+        operation.capitalize(),
+        message,
+        error_code.value,
+        path,
+        subtask_id,
+        client_key
+    )
+
     # The status code here must be always 401 because auth_request module in nginx can only handle HTTP 401.
-    response                     = JsonResponse(data, status = 401)
+    response = JsonResponse(data, status=401)
     response["WWW-Authenticate"] = 'Golem realm="Concent Storage"'
     return response

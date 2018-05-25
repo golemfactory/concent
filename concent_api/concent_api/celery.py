f363ce44e809ee940ffe3cee869d3b1f9281f67b
Radoslaw Wrzesien <radoslaw.wrzesien@codepoets.it>
2018-05-25 20:58:06 +0200

    Remove mechanism for importing only selected Celery tasks

--- concent_api/concent_api/celery.py
+++ concent_api/concent_api/celery.py
@@ -1,8 +1,5 @@
-import importlib
 import os
 from celery import Celery
-
-import django
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'concent_api.settings')
@@ -15,19 +12,6 @@ app = Celery('concent_api')
 # - namespace='CELERY' means all celery-related configuration keys
 #   should have a `CELERY_` prefix.
 app.config_from_object('django.conf:settings', namespace = 'CELERY')
-
-django.setup()
-from django.conf            import settings  # noqa: E402 pylint: disable=wrong-import-position
-from concent_api.constants  import AVAILABLE_CONCENT_FEATURES  # noqa: E402 pylint: disable=wrong-import-position
-
-# Load tasks only from features specified in CONCENT_FEATURES variable
-for feature_name in settings.CONCENT_FEATURES:
-
-    app.conf.imports += tuple(
-        [f'{app}.tasks'
-         for app in AVAILABLE_CONCENT_FEATURES[feature_name]['required_django_apps']
-         if importlib.util.find_spec(f'{app}.tasks') is not None]
-    )
 
 app.conf.task_create_missing_queues = True
 app.conf.task_routes = ([

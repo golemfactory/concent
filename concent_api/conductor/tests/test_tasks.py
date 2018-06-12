e69e1270d9c5c2d55836599f5f2b7ef576e9b05e
Karol Beker <karolbeker@gmail.com>
2018-06-12 17:48:58 +0200

    Fix tests after removing Conductor's use of the Control database

--- concent_api/conductor/tests/test_tasks.py
+++ concent_api/conductor/tests/test_tasks.py
@@ -49,40 +49,52 @@ class CoreTaskTestCase(ConcentIntegrationTestCase):
         )
 
         store_subtask(
-            task_id=self.compute_task_def['task_id'],
-            subtask_id=self.compute_task_def['subtask_id'],
+            task_id=report_computed_task.task_to_compute.compute_task_def['task_id'],
+            subtask_id=report_computed_task.task_to_compute.compute_task_def['subtask_id'],
             provider_public_key=self.PROVIDER_PUBLIC_KEY,
             requestor_public_key=self.REQUESTOR_PUBLIC_KEY,
             state=Subtask.SubtaskState.ADDITIONAL_VERIFICATION,
             next_deadline=get_current_utc_timestamp() + settings.CONCENT_MESSAGING_TIME,
-            task_to_compute=self._get_deserialized_task_to_compute(
-                task_id=self.compute_task_def['task_id'],
-                subtask_id=self.compute_task_def['subtask_id'],
-            ),
+            task_to_compute=report_computed_task.task_to_compute,
             report_computed_task=report_computed_task,
         )
 
         with mock.patch('conductor.tasks.blender_verification_order.delay') as mock_blender_verification_order:
-            upload_acknowledged(self.verification_request.subtask_id)
+            upload_acknowledged(
+                subtask_id=report_computed_task.subtask_id,
+                source_file_size=report_computed_task.task_to_compute.size,
+                source_package_hash=report_computed_task.task_to_compute.package_hash,
+                result_file_size=report_computed_task.size,
+                result_package_hash=report_computed_task.package_hash,
+            )
 
         self.verification_request.refresh_from_db()
 
         self.assertTrue(self.verification_request.upload_acknowledged)
         mock_blender_verification_order.assert_called_once_with(
-            self.verification_request.subtask_id,
-            self.verification_request.source_package_path,
-            report_computed_task.task_to_compute.size,
-            report_computed_task.task_to_compute.package_hash,
-            self.verification_request.result_package_path,
-            report_computed_task.size,          # pylint: disable=no-member
-            report_computed_task.package_hash,  # pylint: disable=no-member
-            self.verification_request.blender_subtask_definition.output_format,
-            self.verification_request.blender_subtask_definition.scene_file,
+            subtask_id=self.verification_request.subtask_id,
+            source_package_path=self.verification_request.source_package_path,
+            source_size=report_computed_task.task_to_compute.size,
+            source_package_hash=report_computed_task.task_to_compute.package_hash,
+            result_package_path=self.verification_request.result_package_path,
+            result_size=report_computed_task.size,
+            result_package_hash=report_computed_task.package_hash,
+            output_format=self.verification_request.blender_subtask_definition.output_format,
+            scene_file=self.verification_request.blender_subtask_definition.scene_file,
         )
 
     def test_that_upload_acknowledged_task_should_log_error_when_verification_request_with_given_subtask_id_does_not_exist(self):
+        report_computed_task = self._get_deserialized_report_computed_task(
+            subtask_id=self.compute_task_def['subtask_id'],
+        )
         with mock.patch('conductor.tasks.logging.error') as mock_logging_error:
-            upload_acknowledged('non_existing_subtask_id')
+            upload_acknowledged(
+                subtask_id='non_existing_subtask_id',
+                source_file_size=report_computed_task.task_to_compute.size,
+                source_package_hash=report_computed_task.task_to_compute.package_hash,
+                result_file_size=report_computed_task.size,
+                result_package_hash=report_computed_task.package_hash,
+            )
 
         self.verification_request.refresh_from_db()
 

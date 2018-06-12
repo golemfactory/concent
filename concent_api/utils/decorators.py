76db34cfed3c81cc89ed5fabb29a5bac957e953a
Jakub89 <jakubczachura@gmail.com>
2018-06-12 14:45:59 +0200

    Add check for golem_messages.message.tasks.TaskMessage if it 'is_valid()'

--- concent_api/utils/decorators.py
+++ concent_api/utils/decorators.py
@@ -19,8 +19,10 @@ from core.validation                import validate_golem_message_signed_with_ke
 from core.validation import get_validated_client_public_key_from_client_message
 from core.exceptions                import ConcentInSoftShutdownMode
 from core.exceptions import ConcentFeatureIsNotAvailable
-from core.exceptions import Http400
 from core.exceptions import FileTransferTokenError
+from core.exceptions import GolemMessageValidationError
+from core.exceptions import HashingAlgorithmError
+from core.exceptions import Http400
 
 from utils.helpers import join_messages
 from utils.shortcuts                import load_without_public_key
@@ -127,7 +129,7 @@ def handle_errors_and_responses(database_name):
                 response_from_view = view(request, client_message, client_public_key, *args, **kwargs)
                 if database_name is not None:
                     transaction.savepoint_commit(sid, using=database_name)
-            except (Http400, FileTransferTokenError) as exception:
+            except (Http400, FileTransferTokenError, HashingAlgorithmError, GolemMessageValidationError) as exception:
                 logging.log_400_error(
                     logger,
                     view.__name__,

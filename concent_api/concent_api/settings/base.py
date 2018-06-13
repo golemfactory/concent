67f25e35d2d85f33dab83a646a05c620e85a4931
MichalW-CP <michal.wyspianski@codepoets.it>
2018-06-13 14:58:04 +0200

    Fix Static Files Collecting

--- concent_api/concent_api/settings/base.py
+++ concent_api/concent_api/settings/base.py
@@ -136,7 +136,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "core", "static")]
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static-root')

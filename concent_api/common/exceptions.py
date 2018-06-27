1186a66068d8430629aeb7ece80a9978b39e1e62
Karol Beker <karolbeker@gmail.com>
2018-06-27 15:55:27 +0200

    Bugfix mypy warnings

--- concent_api/common/exceptions.py
+++ concent_api/common/exceptions.py
@@ -1,4 +1,5 @@
 from typing import Optional
+from typing import Union
 
 from .constants import ErrorCode
 
@@ -14,7 +15,7 @@ class ConcentFeatureIsNotAvailable(Exception):
 class ConcentBaseException(Exception):
 
     def __init__(self, error_message: Optional[str], error_code: ErrorCode) -> None:
-        assert isinstance(error_message, (str, None))
+        assert isinstance(error_message, Union[str, None].__args__)
         assert isinstance(error_code, ErrorCode)
         self.error_code = error_code
         self.error_message = '' if error_message is None else error_message

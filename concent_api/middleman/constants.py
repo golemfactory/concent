2e9182374c324bca5835e9b417b300b20ed14f4d
Piotr Dybowski <piotr.dybowski@codepoets.it>
2018-08-14 13:19:44 +0200

    Add RequestQueueItem and ResponseQueueItem data structures

--- concent_api/middleman/constants.py
+++ concent_api/middleman/constants.py
@@ -1,4 +1,25 @@
+from collections import namedtuple
+
 DEFAULT_INTERNAL_PORT = 9054
 DEFAULT_EXTERNAL_PORT = 9055
 LOCALHOST_IP = "127.0.0.1"
 ERROR_ADDRESS_ALREADY_IN_USE = "Error: address already in use"
+
+RequestQueueItem = namedtuple(
+    "RequestQueueItem",
+    (
+        "connection_id",
+        "request_id",
+        "message",
+        "timestamp",
+    )
+)
+
+ResponseQueueItem = namedtuple(
+    "ResponseQueueItem",
+    (
+        "message",
+        "request_id",
+        "timestamp",
+    )
+)

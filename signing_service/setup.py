f857586d862ad336106ee68204f248824a1c564d
Karol Beker <karolbeker@gmail.com>
2018-08-21 16:13:37 +0200

    Add generating version by get_version function from golem_message repository

--- signing_service/setup.py
+++ signing_service/setup.py
@@ -3,19 +3,12 @@
 import os
 from setuptools import setup
 
-
-def get_version():
-    path = os.path.join(
-        os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
-        'RELEASE-VERSION',
-    )
-    with open(path, 'r') as version_file:
-        return version_file.read()
+from version import get_version
 
 
 setup(
     name='Signing-Service',
-    version=get_version(),
+    version=get_version(prefix='v'),
     url='https://github.com/golemfactory/concent',
     maintainer='Code Poets',
     maintainer_email='contact@codepoets.it',

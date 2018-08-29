7ee8fcca15603a561d5d8150500ed0a2a6acf1cf
Piotr Dybowski <piotr.dybowski@codepoets.it>
2018-08-29 13:14:56 +0200

    Add CONNECTION_COUNTER_LIMIT constant for MiddleMan connections

--- concent_api/middleman/constants.py
+++ concent_api/middleman/constants.py
@@ -4,6 +4,7 @@ DEFAULT_INTERNAL_PORT = 9054
 DEFAULT_EXTERNAL_PORT = 9055
 LOCALHOST_IP = "127.0.0.1"
 ERROR_ADDRESS_ALREADY_IN_USE = "Error: address already in use"
+CONNECTION_COUNTER_LIMIT = 987654321
 
 RequestQueueItem = namedtuple(
     "RequestQueueItem",

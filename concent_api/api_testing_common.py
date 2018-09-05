4eb56a3ff7473a56f6e1cd7d30428c3d13368ba1
pawelkisielewicz <pawel.kisielewicz@codepoets.it>
2018-09-05 13:55:55 +0200

    fixup! Add api test sending multiple messages in one time

--- concent_api/api_testing_common.py
+++ concent_api/api_testing_common.py
@@ -76,7 +76,7 @@ def assert_condition(actual, expected, error_message = None):
 
 def assert_iterables_content_is_equal(actual: Iterable[str], expected: Iterable[str]) -> None:
     if not sorted(actual) == sorted(expected):
-        raise TestAssertionException(f'Content of iterables is not equal. Actual {actual}. Expected {expected}')
+        raise TestAssertionException(f'Content of iterables is not equal. "Actual: {actual}". "Expected: {expected}"')
 
 
 def print_golem_message(message, indent = 4):

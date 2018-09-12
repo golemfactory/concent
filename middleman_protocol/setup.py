928edb7c1d80aa488e45413e4bb583b0e089d550
pawelkisielewicz <pawel.kisielewicz@codepoets.it>
2018-09-12 08:44:28 +0200

    Add generating version in middleman_protocol setup.py

--- middleman_protocol/setup.py
+++ middleman_protocol/setup.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python3
-
+from os import path
+from pathlib import Path
 from setuptools import setup
 
-from version import get_version
+
+def generate_version() -> str:
+    here = path.abspath(path.dirname(__file__))
+    version_file = Path(path.join(here, "RELEASE-VERSION"))
+    if version_file.is_file():
+        return open(version_file, "r").read()
+    else:
+        from version import get_version
+        return get_version(prefix='v')
 
 
 setup(
     name='Middleman-Protocol',
-    version=get_version(prefix='v'),
+    version=generate_version(),
     url='https://github.com/golemfactory/concent',
     maintainer='Code Poets',
     maintainer_email='contact@codepoets.it',

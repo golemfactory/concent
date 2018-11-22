4b32e2c2b9649c691b8781fdb0037ca3027cba80
Jakub89 <jakubczachura@gmail.com>
2018-11-22 16:48:05 +0100

    temporary fix for ETHEREUM_TRANSACTION_HASH_LENGTH

--- concent_api/core/payments/bankster.py
+++ concent_api/core/payments/bankster.py
@@ -266,6 +266,7 @@ def finalize_payment(deposit_claim: DepositClaim) -> Optional[str]:
             assert False
 
         # Bankster puts transaction ID in DepositClaim.tx_hash.
+        ethereum_transaction_hash = adjust_transaction_hash(ethereum_transaction_hash)
         deposit_claim.tx_hash = ethereum_transaction_hash
         deposit_claim.full_clean()
         deposit_claim.save()
@@ -420,6 +421,7 @@ def settle_overdue_acceptances(
             value=requestor_payable_amount,
             payment_ts=cut_off_time,
         )
+        transaction_hash = adjust_transaction_hash(transaction_hash)
 
         # Deposit lock for requestor.
         claim_against_requestor = DepositClaim(
@@ -487,3 +489,12 @@ def discard_claim(deposit_claim: DepositClaim) -> bool:
             except DepositAccount.DoesNotExist:
                 claim_removed = False
     return claim_removed
+
+
+def adjust_transaction_hash(tx_hash):
+    if isinstance(tx_hash, str):
+        if tx_hash[0] == '0' and tx_hash[1] == 'x':
+            tx_hash = tx_hash[2:]
+        return tx_hash
+    else:
+        return None

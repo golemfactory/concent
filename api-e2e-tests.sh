5601653b3b90e2a8cc60adc161776e99eeba741b
Piotr Dybowski <piotr.dybowski@codepoets.it>
2018-11-23 16:02:33 +0100

    Change script for running e2e tests: add default value for target: "http://127.0.0.1:8000"

--- api-e2e-tests.sh
+++ api-e2e-tests.sh
@@ -1,8 +1,14 @@
 #!/bin/bash -e
 
+if [ $# -gt 0 ]; then
+    target=$1
+else
+    target="http://127.0.0.1:8000"
+fi
+
 printf "=================== RUNNING API INTEGRATION TESTS ====================\n"
-python3 concent_api/api-e2e-force-accept-or-reject-results-test.py $1
-python3 concent_api/api-e2e-force-get-task-result-test.py $1
-python3 concent_api/api-e2e-force-payment.py $1
-python3 concent_api/api-e2e-force-report-computed-task-test.py $1
-python3 concent_api/api-e2e-additional-verification-test.py $1
+python3 concent_api/api-e2e-force-accept-or-reject-results-test.py $target
+python3 concent_api/api-e2e-force-get-task-result-test.py $target
+python3 concent_api/api-e2e-force-payment.py $target
+python3 concent_api/api-e2e-force-report-computed-task-test.py $target
+python3 concent_api/api-e2e-additional-verification-test.py $target

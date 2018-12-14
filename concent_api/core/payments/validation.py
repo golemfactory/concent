13c91bd4a8aac7d99eae48dbbc326e4847904bd6
pawelkisielewicz <pawel.kisielewicz@codepoets.it>
2018-12-14 07:19:28 +0100

    !fixup Add function to compare payment_ts and payment closure times

--- concent_api/core/payments/validation.py
+++ concent_api/core/payments/validation.py
@@ -3,18 +3,16 @@ from core.payments.backends.sci_backend import get_list_of_payments
 from core.payments.backends.sci_backend import TransactionType
 
 
-def validate_that_last_closure_time_is_older_than_oldest_payment(  # pylint: disable=inconsistent-return-statements
+def validate_that_last_closure_time_is_older_than_oldest_payment(
     requestor_eth_address: str,
     provider_eth_address: str,
-    youngest_payment_ts: int,
+    search_payments_since_ts: int,
 ) -> None:
     forced_payment_event_list = get_list_of_payments(
         requestor_eth_address=requestor_eth_address,
         provider_eth_address=provider_eth_address,
-        payment_ts=youngest_payment_ts,
+        payment_ts=search_payments_since_ts,
         transaction_type=TransactionType.FORCE,
     )
-    if forced_payment_event_list == []:
-        return None
-    elif youngest_payment_ts < max([event.closure_time for event in forced_payment_event_list]):
+    if forced_payment_event_list != [] and search_payments_since_ts < max([event.closure_time for event in forced_payment_event_list]):
         raise PaymentTimestampError

b5d027c4e1bd1d0f7bb8b2293cf40fea133b215e
pawelkisielewicz <pawel.kisielewicz@codepoets.it>
2019-01-15 21:06:41 +0100

    Remove get_or_create_with_retry and retry outside of transaction

--- concent_api/api-e2e-multi-threads-test.py
+++ concent_api/api-e2e-multi-threads-test.py
@@ -3,8 +3,8 @@ import os
 import sys
 import time
 from typing import Optional
-import requests
 
+import requests
 from golem_messages import message
 from golem_messages.factories.tasks import ReportComputedTaskFactory
 from golem_messages.message.concents import ForceGetTaskResult
@@ -12,23 +12,31 @@ from golem_messages.message.concents import ForceReportComputedTask
 from golem_messages.message.tasks import AckReportComputedTask
 from golem_messages.message.tasks import ReportComputedTask
 
-from api_testing_common import call_function_in_threads
-from api_testing_common import count_fails, assert_content_equal
 from api_testing_common import PROVIDER_PRIVATE_KEY
 from api_testing_common import REQUESTOR_PRIVATE_KEY
 from api_testing_common import api_request
+from api_testing_common import assert_content_equal
+from api_testing_common import calculate_deadline
+from api_testing_common import calculate_timestamp
+from api_testing_common import call_function_in_threads
+from api_testing_common import count_fails
+from api_testing_common import create_ack_report_computed_task
+from api_testing_common import create_force_subtask_results
+from api_testing_common import create_signed_report_computed_task
 from api_testing_common import create_signed_task_to_compute
 from api_testing_common import run_tests
 from api_testing_common import timestamp_to_isoformat
 from common.helpers import get_current_utc_timestamp
 from common.helpers import sign_message
+from common.testing_helpers import generate_ecc_key_pair
+from protocol_constants import ProtocolConstants
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "concent_api.settings")
 
 REPORT_COMPUTED_TASK_SIZE = 10
 
 NUMBER_OF_TESTING_THREADS = 3  # changing this variable remember to correct number of expected responses
-MAXIMUM_WAITING_TIME_FOR_ALL_RESPONSES = 3  # seconds
+MAXIMUM_WAITING_TIME_FOR_ALL_RESPONSES = 6  # seconds
 
 responses_global = []  # type: list
 
@@ -119,6 +127,32 @@ def send_correct_force_get_task_result(
     responses_global.append(response.__class__.__name__)
 
 
+def send_correct_force_subtask_results(
+    cluster_url: str,
+    report_computed_task: ReportComputedTask,
+    current_time: int,
+    provider_private_key: bytes,
+    requestor_private_key: bytes,
+    expected_status: Optional[int] = None,
+) -> None:
+    response = api_request(
+        cluster_url,
+        'send',
+        provider_private_key,
+        CONCENT_PUBLIC_KEY,
+        create_force_subtask_results(
+            timestamp=timestamp_to_isoformat(current_time),
+            ack_report_computed_task=create_ack_report_computed_task(
+                timestamp=timestamp_to_isoformat(current_time),
+                report_computed_task=report_computed_task,
+                requestor_private_key=requestor_private_key
+            )
+        ),
+        expected_status=expected_status,
+    )
+    responses_global.append(response.__class__.__name__)
+
+
 @count_fails
 def test_case_multiple_requests_concerning_one_subtask_will_be_processed_one_by_one_if_subtask_exists_in_database(
     cluster_consts: str,
@@ -185,6 +219,53 @@ def test_case_multiple_force_get_task_result_concerning_one_subtask_will_be_proc
             break
 
     expected_responses = ['AckForceGetTaskResult', 'ServiceRefused', 'ServiceRefused']
+    assert len(expected_responses) == NUMBER_OF_TESTING_THREADS, 'Did you change number of testing threads and forget to change expected_responses?'
+
+    print('Responses = ' + str(responses_global))
+    assert_content_equal(actual=responses_global, expected=expected_responses)
+    print('Single test passed successfully')
+
+
+@count_fails
+def test_case_multiple_force_subtask_results_does_not_cause_integrity_errors(
+    cluster_consts: ProtocolConstants,
+    cluster_url: str
+) -> None:
+    clear_responses()
+    provider_private_key, provider_public_key = generate_ecc_key_pair()
+    requestor_private_key, requestor_public_key = generate_ecc_key_pair()
+    current_time = get_current_utc_timestamp()
+    signed_task_to_compute = create_signed_task_to_compute(
+        timestamp=calculate_timestamp(current_time, cluster_consts.concent_messaging_time, cluster_consts.minimum_upload_rate),
+        deadline=calculate_deadline(current_time, cluster_consts.concent_messaging_time, cluster_consts.minimum_upload_rate),
+        price=1000,
+        provider_public_key=provider_public_key,
+        provider_private_key=provider_private_key,
+        requestor_public_key=requestor_public_key,
+        requestor_private_key=requestor_private_key
+    )
+    report_computed_task = create_signed_report_computed_task(
+        task_to_compute=signed_task_to_compute,
+        provider_private_key=provider_private_key,
+    )
+
+    call_function_in_threads(
+        func=send_correct_force_subtask_results,
+        number_of_threads=NUMBER_OF_TESTING_THREADS,
+        cluster_url=cluster_url,
+        report_computed_task=report_computed_task,
+        current_time=current_time,
+        provider_private_key=provider_private_key,
+        requestor_private_key=requestor_private_key,
+    )
+
+    end_time = time.time() + MAXIMUM_WAITING_TIME_FOR_ALL_RESPONSES
+    while len(responses_global) != NUMBER_OF_TESTING_THREADS:
+        time.sleep(0.1)
+        if time.time() >= end_time:
+            break
+
+    expected_responses = ['ServiceRefused', 'ServiceRefused', 'ServiceRefused']
     assert len(expected_responses) == NUMBER_OF_TESTING_THREADS, 'Did you change number of testing threads and forget to change expected_responses?'
 
     print('Responses = ' + str(responses_global))
--- concent_api/core/model_helpers.py
+++ concent_api/core/model_helpers.py
@@ -3,11 +3,13 @@ from typing import Optional
 from typing import Type
 from typing import Union
 
+from django.core.exceptions import ValidationError
 from django.db import IntegrityError
 from django.db.models import Model
 from django.db.models import QuerySet
 from django.db.models.base import ModelBase
 from psycopg2 import errorcodes as pg_errorcodes
+from common.decorators import non_nesting_atomic
 
 
 def get_one_or_none(
@@ -26,14 +28,20 @@ def get_one_or_none(
 
 def get_or_create_with_retry(model: Type[Model], **kwargs: Any) -> Model:
     try:
-        model_object = model.objects.get_or_create_full_clean(
-            **kwargs,
-        )
+        with non_nesting_atomic(using='control'):
+            model_object = model.objects.get_or_create_full_clean(
+                **kwargs,
+            )
     except IntegrityError as exception:
-        if exception.pgcode == pg_errorcodes.UNIQUE_VIOLATION:
+        if exception.__cause__.pgcode == pg_errorcodes.UNIQUE_VIOLATION:
             model_object = model.objects.get_or_create_full_clean(
                 **kwargs,
             )
         else:
             raise
+    except ValidationError:
+        with non_nesting_atomic(using='control'):
+            model_object = model.objects.get_or_create_full_clean(
+                **kwargs,
+            )
     return model_object

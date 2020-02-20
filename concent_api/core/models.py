34d9f8659ea0372ea9c0bc9c303333caf61d169b
Jakub89 <jakub.czachura@codepoets.it>
2020-02-20 15:35:01 +0100

    Remove TTC validation from Subtask.clean()

--- concent_api/core/models.py
+++ concent_api/core/models.py
@@ -32,7 +32,6 @@ from common.constants import ConcentUseCase
 from common.exceptions import ConcentInSoftShutdownMode
 from common.fields import Base64Field
 from common.fields import ChoiceEnum
-from common.helpers import deserialize_database_message
 from common.helpers import deserialize_message
 from common.helpers import parse_datetime_to_timestamp
 
@@ -42,7 +41,6 @@ from .constants import ETHEREUM_TRANSACTION_HASH_LENGTH
 from .constants import GOLEM_PUBLIC_KEY_LENGTH
 from .constants import MESSAGE_TASK_ID_MAX_LENGTH
 from .constants import TASK_OWNER_KEY_LENGTH
-from .validation import validate_database_task_to_compute
 
 
 class SubtaskWithTimingColumnsManager(Manager):
@@ -520,25 +518,6 @@ class Subtask(Model):
             raise ValidationError({
                 'computation_deadline': "TaskToCompute deadline mismatch"
             })
-
-        # Validation for every nested message which is stored in Control database
-        # Every nested message must be the same as message stored separately.
-        MESSAGES_TO_VALIDATE_TASK_TO_COMPUTE = [
-            self.report_computed_task,
-            self.subtask_results_accepted,
-            self.reject_report_computed_task,
-            self.ack_report_computed_task,
-            self.subtask_results_rejected,
-            self.force_get_task_result,
-            self.subtask_results_accepted,
-        ]
-
-        for task_to_compute_to_validate in MESSAGES_TO_VALIDATE_TASK_TO_COMPUTE:
-            if task_to_compute_to_validate is not None:
-                validate_database_task_to_compute(
-                    task_to_compute=deserialized_task_to_compute,
-                    message_to_compare=deserialize_database_message(task_to_compute_to_validate),
-                )
 
         for related_message_name in Subtask.MESSAGE_FOR_FIELD:
             related_message = getattr(self, related_message_name)
